5812833ff6272ecf02f7ae2985ed744c397b4429
lavoiee <ericjohnlavoie@gmail.com>
2017-01-24 19:57:51 -0500

    Added DisplayReset() in a couple places.

--- NumberGuessingGame/NumberGuessingGame/Program.cs
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -4,57 +4,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NumberGuessingGame
+namespace TheNumberGuessingGame
 {
-    // Combined branch
     class Program
     {
         //
         // Defining constant global variables
         //
+
         private const int MAX_NUMBER_OF_PLAYER_GUESSES = 4;
         private const int MAX_NUMBER_TO_GUESS = 10;
 
         //
         // Defining other global variables
         //
+
         private static int playersGuess;
         private static int numberToGuess;
         private static int roundNumber;
+        private static int gameNumber;
         private static int numberOfWins;
         private static int numberOfLosses;
-        private static int numberOfCurrentPlayerGuess;
         private static int[] numbersPlayerHasGuessed = new int[MAX_NUMBER_OF_PLAYER_GUESSES];
         private static bool playingGame;
         private static bool playingRound;
-        private static bool numberGuessedCorrectly;
         private static Random random = new Random();
 
         static void Main(string[] args)
         {
-            //
-            // Display welcome screen, query user to play or not, initialize game variables.
-            //
-            playingGame = DisplayWelcomeScreen();
-            InitializeGame();
+            playingGame = DisplayWelcomeScreen();            
+                
+            InitializeGame();      
+            
             DisplayClosingScreen();
-
+            
         }
 
         public static bool DisplayWelcomeScreen()
         {
             Console.WriteLine("Welcome to the Number Guessing Game!");
             Console.WriteLine();
-            Console.WriteLine("Would you like to play? Type yes or no");
             Console.WriteLine();
-            string userResponse = Console.ReadLine().ToUpper();
+            Console.WriteLine();
 
-            if (userResponse == "YES")
-            {
-                playingGame = true;
-            }
+            //
+            // ask user if they want to play the game, validate for yes/no
+            //
+
+            playingGame = ValidateYesNo("Would you like to play?");
+            
 
             return playingGame;
+        }
+
+        private static bool ValidateYesNo(string prompt)
+        {
+            bool validResponse = false;
+            bool userSaidYes = false;
+
+            while (!validResponse)
+            {
+                DisplayReset();
+                Console.Write($"{prompt} Type yes or no: ");
+                //Console.WriteLine();
+                string userResponse = Console.ReadLine().ToUpper();
+
+                if (userResponse == "YES")
+                {
+                    userSaidYes = true;
+                    validResponse = true;
+                }
+                else if (userResponse == "NO")
+                {
+                    validResponse = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please type exactly \"yes\" or \"no\".");
+                    validResponse = false;
+                }
+            }
+
+            return userSaidYes;
         }
 
         public static void DisplayRulesScreen()
@@ -63,180 +94,276 @@ namespace NumberGuessingGame
             Console.WriteLine("The user will guess one integer at a time, up to four times");
             Console.WriteLine("The computer will display if the guess is too low, too high");
             Console.WriteLine("or correct.");
+            Console.WriteLine();
+            DisplayContinuePrompt();
 
-            Console.WriteLine("Press any key to continue.");
+            DisplayReset();
+            
 
-            Console.ReadKey();
         }
 
         public static void InitializeGame()
         {
-            
-            DisplayReset();
-            roundNumber = 0;
-            DisplayRulesScreen();
+            //
+            // Main control loop for game
+            //
 
-            //
-            // Main control loop for game.
-            //
             while (playingGame)
             {
-                InitializeRound();
-                while (!playingRound)
+                DisplayReset();
+
+                //
+                // Count the games so that rule screen is only shown for first game
+                //
+
+                gameNumber += 1;
+
+                if (gameNumber <= 1)
                 {
-                    DisplayGetPlayerGuessScreen();
-                    DisplayPlayerGuessFeedback();
-                    UpdateAndDisplayRoundStatus();
-                    
+                    DisplayRulesScreen();
+                }           
+
+                InitializeRound();
+                
+                playingRound = true;
+                
+                for (int round = 0; round < MAX_NUMBER_OF_PLAYER_GUESSES; round++)
+                {//cycles through four rounds, unless playingRound becomes false
+                 //(if they win or choose to quit)
+                    if (playingRound)
+                    {
+                        DisplayGetPlayerGuessScreen();
+
+                        DisplayPlayerGuessFeedback();
+
+                        UpdateAndDisplayRoundStatus();
+                        
+                        //DisplayContinueQuitRoundPrompt();
+                    }                    
                 }
-            
-            DisplayPlayerStats();    
+
+
+                DisplayReset();
+                DisplayContinueQuitGamePrompt();
+                                
             }
-            DisplayClosingScreen();
         }
 
         public static void InitializeRound()
         {
-            DisplayReset();
             numberToGuess = GetNumberToGuess();
-        }
 
+            roundNumber = 1;
+
+            //
+            // reset the array to zero, so that round 2 and up have clean slates
+            //            
+
+            for (int index = 0; index < MAX_NUMBER_OF_PLAYER_GUESSES; index++)
+            {
+                numbersPlayerHasGuessed[index] = 0;
+            }
+            
+        }
+        
         public static int GetNumberToGuess()
-        {
-            return random.Next(0, 11);
+        {            
+            return random.Next(1, MAX_NUMBER_TO_GUESS);
         }
         
         public static int DisplayGetPlayerGuessScreen()
         {
-            //
-            // 
-            //
-            Console.WriteLine("Enter the number of your guess as an integer: ");
-            int.TryParse(Console.ReadLine(), out playersGuess);
-            numbersPlayerHasGuessed[roundNumber] = playersGuess;
+            DisplayReset();
 
-            //Console.WriteLine("Player guess from array: " + numbersPlayerHasGuessed[roundNumber]);
+            //
+            // Display previous guesses for rounds 2 and up
+            //
+
+            //if (roundNumber > 1)
+            //{
+            //    DisplayNumbersGuessed();
+            //}
+
+            //
+            // ask player for guess and validate for integer between 1 and 10
+            //
+
+            playersGuess = ValidateInteger("the number of your guess", 1, 10);
+            
+
+            //
+            // store player guess in array
+            //
+
+            numbersPlayerHasGuessed[roundNumber - 1] = playersGuess;
+            //"roundNumber - 1" allows the index to use the 0 start
+            //while letting the user see the roundNumber as 1 start
+
+            //
+            // display player's guess to them
+            //
+
+            Console.WriteLine($"Your guess: {numbersPlayerHasGuessed[roundNumber - 1]}");
+            
+
             return playersGuess;
         }
-        
+
+        private static int ValidateInteger(string integerNeeded, int minValue, int maxValue)
+        {
+            bool validResponse = false;
+
+            string userResponse;
+            int userInteger = 0;
+
+            while (!validResponse)
+            {
+                DisplayReset();
+                DisplayNumbersGuessed();
+                Console.Write($"Please enter {integerNeeded} as an integer ({minValue} - {maxValue}): ");
+                userResponse = Console.ReadLine();
+                if (int.TryParse(userResponse, out userInteger))
+                {
+                    if (userInteger >= minValue && userInteger <= maxValue)
+                    {
+                        validResponse = true;
+                    }
+                    else
+                    {
+                        DisplayReset();
+                        Console.WriteLine($"Please enter a valid integer between {minValue} and {maxValue}!");
+                        DisplayContinuePrompt();
+                        Console.Clear();
+                    }
+                }
+                else
+                {
+                    DisplayReset();
+                    Console.WriteLine($"Please only enter a whole number between {minValue} and {maxValue}!");
+                    DisplayContinuePrompt();
+                    Console.Clear();                    
+                }
+                
+            }
+
+            return userInteger;
+        }
+
         public static void DisplayPlayerGuessFeedback()
         {
             DisplayReset();
-            roundStats();
-            if (roundNumber < MAX_NUMBER_OF_PLAYER_GUESSES - 1)
+            
+            if (playersGuess == numberToGuess)
             {
-                if (playersGuess == numberToGuess)
-                {
-                    Console.WriteLine("You guessed: " + playersGuess);
-                    Console.WriteLine("Congratulations! You guessed the secret number.");                    
-                    DisplayContinuePrompt();
-                    playingRound = true;
-                }
-                else if (playersGuess < numberToGuess)
-                {
-                    Console.WriteLine("I'm sorry. You guessed too low.");
-                    DisplayContinuePrompt();
-                }
-                else
-                {
-                    Console.WriteLine("I'm sorry. You guessed too high.");
-                    DisplayContinuePrompt();
-                }
+                Console.WriteLine($"That's right! The number was {numberToGuess}");
+            }
+            else if (playersGuess < numberToGuess)
+            {
+                Console.WriteLine($"Sorry, {playersGuess} is too low.");
             }
             else
             {
-                if (playersGuess == numberToGuess)
-                {
-                    Console.WriteLine("You guessed: " + playersGuess);
-                    Console.WriteLine("Congratulations! You guessed the secret number.");
-                    DisplayContinuePrompt();
-                    playingRound = true;
-                }
-                else
-                {
-                    DisplayLoseScreen();
-                    playingRound = true;
-                }                               
-            }           
-        }
-        
-        public static void DisplayLoseScreen()
-        {
-            Console.WriteLine("I'm sorry you've run out of chances.");
-            DisplayContinuePrompt();
+                Console.WriteLine($"Sorry, {playersGuess} is too high.");
+            }
         }
 
         public static int UpdateAndDisplayRoundStatus()
         {
-            numberOfCurrentPlayerGuess += 1;
-            //Console.WriteLine($"Number of current guess: {numberOfCurrentPlayerGuess}");
-                if ((playersGuess == numberToGuess))
-                {
-                    numberOfWins += 1;               
-                    playingRound = true;
-                }
-                if (roundNumber < MAX_NUMBER_OF_PLAYER_GUESSES - 1)
-                {
-
-                }
-                else
-                {
-                    playingRound = true;
-                    numberOfLosses += 1;
-                
-                }
-            roundNumber += 1;
-            roundStats();
+            if (playersGuess == numberToGuess)
+            {                
+                playingRound = false;
+                Console.WriteLine("Increasing your number of wins by 1.");
+                Console.WriteLine();
+                numberOfWins += 1;
+            }
+            else if (playersGuess != numberToGuess && roundNumber < MAX_NUMBER_OF_PLAYER_GUESSES)
+            {
+                numbersPlayerHasGuessed[roundNumber - 1] = playersGuess;
+                Console.WriteLine("Storing your incorrect guess in array.");
+                Console.WriteLine();
+                roundNumber += 1;
+            }
+            else if (playersGuess != numberToGuess && roundNumber >= MAX_NUMBER_OF_PLAYER_GUESSES)
+            {
+                playingRound = false;
+                Console.WriteLine("Guesses used up. Ending game.");
+                Console.WriteLine("Increasing your number of losses by 1.");
+                Console.WriteLine();
+                numberOfLosses += 1;
+            }
+            DisplayContinuePrompt();
             return roundNumber;
         }
 
-        public static void roundStats()
+        public static void DisplayRoundStats()
         {
             DisplayReset();
-            Console.WriteLine("Rounds Played: " + roundNumber);
-            Console.WriteLine("Current Random number: " + numberToGuess);
+            Console.WriteLine($"Rounds Completed: {roundNumber - 1}");
             DisplayNumbersGuessed();
-        }
-
-        public static void DisplayContinuePrompt()
-        {
-            Console.WriteLine("Press any key to continue.");
-            Console.ReadKey();
         }
 
         public static void DisplayPlayerStats()
         {
-            int total;
-            double percentageOfWins;
             DisplayReset();
-            Console.WriteLine($"Round number: {roundNumber}");
-            Console.WriteLine($"Number of wins: {numberOfWins}");
-            Console.WriteLine($"Number of losses: {numberOfLosses}");
+            
+            Console.WriteLine($"Number of Wins: {numberOfWins}");
+            Console.WriteLine($"Number of Losses: {numberOfLosses}");
 
             //
-            // Parse the following output to display the percentage.
+            // Calculate and display percentage of wins:
             //
-            total = numberOfWins + numberOfLosses;
-            percentageOfWins = (double)numberOfWins/total;
-            Console.WriteLine($"Percentage of wins:{percentagizer(percentageOfWins)}%");
-            //Console.WriteLine($"Percentage of wins: {percentageOfWins}");
-            DisplayContinuePrompt();
-            DisplayContinueQuitPrompt();
+
+            int total = numberOfLosses + numberOfWins;
+            double percentageOfWins = (double)numberOfWins / total;
+            Console.WriteLine($"Percentage of wins: {percentagizer(percentageOfWins)}%");
+            Console.WriteLine();
+            Console.WriteLine();
             
+
         }
         //
         // Return a percentage from a decimal value
         //
-        public static double percentagizer(double percent)
+        private static double percentagizer(double percent)
         {
-            double percentage = percent;
-            percentage *= 100.0;
-            return percentage;
+            return percent *= 100.0;
         }
 
+        public static void DisplayContinueQuitRoundPrompt()
+        {            
+            if (playingRound) //this "if" statement will stop the game from asking if they want to  
+                              //play another round if they already lost the game or won the game
+            {
+                DisplayReset();
+
+                //
+                // ask and validate if user wants to play another round
+                //
+
+                bool continueRound = ValidateYesNo("Would you like to play another round?");
+
+                if (continueRound)
+                {
+                    playingRound = true; //updating playingRound here allows the player to move
+                                         //on to the next round
+                                         //I know, it's silly to define it twice. But this way,
+                                         //it is checking both for player's CHOICE to move on or not
+                                         //as well as the ABILITY to move on or not based on
+                                         //if they have already lost or won, as calculated in
+                                         //a previous method
+                }
+                else
+                {
+                    playingRound = false;
+                    playingGame = false;
+                }              
+               
+            }
+        }
         public static void DisplayClosingScreen()
         {
-            DisplayReset();
+            DisplayReset(); 
+
             Console.WriteLine("Thank you for playing!");
 
             DisplayContinuePrompt();
@@ -244,55 +371,37 @@ namespace NumberGuessingGame
 
         public static void DisplayNumbersGuessed()
         {
-            Console.Write("Player's Guesses: ");
-            foreach (int playerGuess in numbersPlayerHasGuessed)
+            Console.Write($"Previous guesses: ");
+            for (int round = 0; round < roundNumber - 1; round++)
             {
-                Console.Write(playerGuess + " ");
+                Console.Write(numbersPlayerHasGuessed[round] + " ");
             }
             Console.WriteLine();
         }
-        
-
-        public static void DisplayContinueQuitPrompt()
-        {
-            string userResponse;
-            DisplayReset();
-            Console.WriteLine("Would you like to continue? YES or NO:");
-            userResponse = Console.ReadLine().ToUpper();
-
-            if (userResponse == "YES")
-            {
-                playingGame = true;
-                resetNumbersPlayerHasGuessed();
-                InitializeGame();
-            }
-            else
-            {
-                playingGame = false;
-            }
-            DisplayContinuePrompt();
-        }
-
-        public static void resetNumbersPlayerHasGuessed()
-        {
-            playingRound = false;
-            playersGuess = 0;
-            for (int i = 0; i <= (MAX_NUMBER_OF_PLAYER_GUESSES -1); i++)
-            {
-                numbersPlayerHasGuessed[i] = 0;
-                //Console.WriteLine(numbersPlayerHasGuessed[i]);
-            }
-
-            //Console.WriteLine("Test:");
-            //Console.WriteLine("Displaying elements of array after reset:");
-            //DisplayNumbersGuessed();
-            DisplayContinuePrompt();
-        }
-
         public static void DisplayReset()
         {
             Console.Clear();
-            Console.WriteLine("THE NUMBER GUESSING GAME");          
+            Console.WriteLine("The Number Guessing Game");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
         }
+
+        public static void DisplayContinueQuitGamePrompt()
+        {
+            DisplayReset();
+
+            DisplayPlayerStats();
+
+            playingGame = ValidateYesNo("Would you like to play another game?");
+        }
+
+        public static void DisplayContinuePrompt()
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+        
+        
     }
 }
