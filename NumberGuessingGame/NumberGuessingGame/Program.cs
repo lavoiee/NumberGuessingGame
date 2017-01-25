8a82e29afdfc5e94a7e0b4f5888c04d946c72070
lavoiee <ericjohnlavoie@gmail.com>
2017-01-24 20:09:49 -0500

    Final

--- NumberGuessingGame/NumberGuessingGame/Program.cs
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -318,6 +318,7 @@ namespace TheNumberGuessingGame
             Console.WriteLine($"Percentage of wins: {percentagizer(percentageOfWins)}%");
             Console.WriteLine();
             Console.WriteLine();
+
             DisplayContinuePrompt();
 
         }
